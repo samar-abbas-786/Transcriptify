76226bd56c4b67989389ab171342d50b657e9d02
Samar Abbas <samarabbas172003@gmail.com>
2025-07-09 23:02:17 +0530

    chnage metadatas

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
 app.post("/getTranscript", async (req, res) => {
   const { videoId } = req.body;
   console.log("Extracted videoId:", videoId);
